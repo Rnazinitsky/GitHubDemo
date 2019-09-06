0e3e9205b1ed60ab01ee82203053149fd6246521
rnazinitsky <rnazinitsky@mail.usf.edu>
2019-09-05 21:00:19 -0400

    Added a smiley face and matched the video demo.

--- VisualStudio2019GitHubDemo/Program.cs
+++ VisualStudio2019GitHubDemo/Program.cs
@@ -6,9 +6,20 @@ namespace VisualStudio2019GitHubDemo
     {
         static void Main(string[] args)
         {
+            
             Console.WriteLine("Hello World!");
+            Console.WriteLine("  000000  ");
+            Console.WriteLine("0000000000");
+            Console.WriteLine("0   00   0");
+            Console.WriteLine("0   00   0");
+            Console.WriteLine("0000000000");
+            Console.WriteLine("0  0000  0");
+            Console.WriteLine("00      00");
+            Console.WriteLine("000    000");
+            Console.WriteLine("  000000  ");
+            //pause to display hello world
             Console.WriteLine("Press any key to continue ...");
-            Console.ReadKey();
+            Console.ReadKey(true);
         }
     }
 }
